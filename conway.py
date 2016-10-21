eb4bc2b4d5b62acc1e3e1530c20781382b12fbce
Matthew Abrams <mcabrams1@gmail.com>
2016-10-20 23:24:18 -0700

    Flesh out World and Cell classes

--- conway.py
+++ conway.py
@@ -1,13 +1,45 @@
 class World():
     def __init__(self):
-        self.living_cells = []
+        self.cells = []
+
+    @property
+    def _living_cells(self):
+        return [cell for cell in self.cells if cell.is_alive]
+
+    @property
+    def living_cell_coordinates(self):
+        return [lc.coordinates for lc in self._living_cells]
+
+    def set_dead_at(self, x, y):
+        cells_to_kill = [c for c in self.cells if c.is_at(x, y)]
+
+        for cell in cells_to_kill:
+            self.cells.remove(cell)
 
     def set_living_at(self, x, y):
-        self.living_cells.append((x, y))
+        self.cells.append(Cell(x, y))
 
     def is_alive_at(self, x, y):
-        return (x, y) in self.living_cells
+        return (x, y) in self.living_cell_coordinates
 
     @property
     def is_empty(self):
-        return len(self.living_cells) == 0
+        return len(self._living_cells) == 0
+
+
+class Cell():
+    def __init__(self, x, y, alive=True):
+        self.x = x
+        self.y = y
+        self.alive = alive
+
+    def is_at(self, x, y):
+        return self.coordinates == (x, y)
+
+    @property
+    def coordinates(self):
+        return (self.x, self.y)
+
+    @property
+    def is_alive(self):
+        return self.alive
--- test_conway.py
+++ test_conway.py
@@ -1,6 +1,6 @@
 import unittest
 
-from conway import World
+from conway import Cell, World
 
 class WorldTestCase(unittest.TestCase):
     def test_a_new_world_is_empty(self):
@@ -20,5 +20,31 @@ class WorldTestCase(unittest.TestCase):
         world = World()
         world.set_living_at(1, 1)
         self.assertTrue(world.is_alive_at(1, 1))
+
+    def test_a_cell_can_be_set_dead_at_coordinate(self):
+        world = World()
+        world.set_living_at(1, 1)
+        world.set_dead_at(1, 1)
+        self.assertFalse(world.is_alive_at(1, 1))
+
+
+class CellTestCase(unittest.TestCase):
+    def test_a_new_cell_is_alive(self):
+        cell = Cell(0, 0)
+        self.assertTrue(cell.is_alive)
+
+    def test_a_cell_can_be_set_dead(self):
+        cell = Cell(0, 0, alive=False)
+        self.assertFalse(cell.is_alive)
+
+    def test_can_set_cells_coordinates(self):
+        for coordinates in [(0, 0), (10, 10)]:
+            cell = Cell(*coordinates)
+            self.assertEqual(cell.coordinates, coordinates)
+
+    def test_cell_is_at_coordinate(self):
+        for coordinates in [(0, 0), (10, 10)]:
+            cell = Cell(*coordinates)
+            self.assertTrue(cell.is_at(*coordinates))
 
 unittest.main()
