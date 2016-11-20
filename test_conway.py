abb44ad8829615b8ad4f48df11557661908896d7
Matthew Abrams <mcabrams1@gmail.com>
2016-11-20 12:20:34 -0800

    Refactor tests to use Cell constants
    - Eliminate magic numbers where possible.

--- test_conway.py
+++ test_conway.py
@@ -137,10 +137,6 @@ class CellTestCase(unittest.TestCase):
         cell = Cell(alive=False)
         self.assertFalse(cell.is_alive)
 
-    def test_cell_is_dead_next_generation_if_fewer_than_2_live_neighbors(self):
-        cell = Cell()
-        self.assertFalse(cell.is_alive_next_generation(1))
-
     def test_cell_is_still_alive_next_gen_if_in_stable_neighborhood(self):
         for i in Cell.STABLE_NEIGHBOR_RANGE:
             with self.subTest(stable_neighbor_count=i):
@@ -148,16 +144,35 @@ class CellTestCase(unittest.TestCase):
                 cell.is_alive_next_generation(i)
                 self.assertTrue(cell.is_alive_next_generation)
 
-    def test_cell_is_dead_from_overcrowding_next_gen_if_4_neighbors(self):
+    def test_cell_is_dead_next_generation_if_lt_stable_count(self):
         cell = Cell()
-        self.assertFalse(cell.is_alive_next_generation(4))
+        min_stable = Cell.STABLE_NEIGHBOR_RANGE[0]
+        is_alive_next_gen = cell.is_alive_next_generation(min_stable - 1)
+        self.assertFalse(is_alive_next_gen)
 
-    def test_lone_dead_cell_is_dead_next_generation(self):
+    def test_cell_is_dead_from_overcrowding_next_gen_if_gt_stable_count(self):
+        cell = Cell()
+        max_stable = Cell.STABLE_NEIGHBOR_RANGE[-1]
+        is_alive_next_gen = cell.is_alive_next_generation(max_stable + 1)
+        self.assertFalse(is_alive_next_gen)
+
+    def test_dead_cell_in_fertile_neighboorhood_is_alive_next_generation(self):
         cell = Cell(alive=False)
-        self.assertFalse(cell.is_alive_next_generation(0))
+        is_alive_next_generation = cell.is_alive_next_generation(
+                Cell.FERTILE_NEIGHBOR_COUNT)
+        self.assertTrue(is_alive_next_generation)
 
-    def test_dead_cell_with_three_living_neighbors_is_alive_next_gen(self):
-        cell = Cell()
-        self.assertTrue(cell.is_alive_next_generation(3))
+    def test_dead_cell_is_dead_next_gen_if_lt_fertile_neighbor_count(self):
+        cell = Cell(alive=False)
+        lt_fertile_count = Cell.FERTILE_NEIGHBOR_COUNT - 1
+        is_alive_next_gen = cell.is_alive_next_generation(lt_fertile_count)
+        self.assertFalse(is_alive_next_gen)
+
+    def test_dead_cell_is_dead_next_gen_if_gt_fertile_neighbor_count(self):
+        cell = Cell(alive=False)
+        gt_fertile_count = Cell.FERTILE_NEIGHBOR_COUNT + 1
+        is_alive_next_gen = cell.is_alive_next_generation(gt_fertile_count)
+        self.assertFalse(is_alive_next_gen)
+
 
 unittest.main()
